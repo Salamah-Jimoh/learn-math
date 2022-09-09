e3a620dbf61b287e0a8e3c8d0526ead78095ee9f
salamah <salamahjimoh@gmail.com>
2022-09-09 15:39:19 -0700

    First Commit

---
+++ readme.md
@@ -0,0 +1,19 @@
+# LearnMath - A web Application to Help Kids Learn Basic Arithmetric Operations
+### Description
+This app will help children improve on their mathematics skill by giving them several equations on addition, subtraction, multiplication and division operations. It is designed with colors that appeals to kids and interactive with soundtracks played when an option is answered correctly or wrongly.
+![](./screenshot.jpg)
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Javascript
+
+## Author
+
+- Salamah-(https://salamah.netlify.com)
---
+++ script/divide.js
@@ -0,0 +1,58 @@
+/*var myNumber= 581;
+var length= myNumber.toString().split('').slice(0,2);
+console.log(length)*/
+
+let optionOne = document.getElementById('option1');
+let optionTwo = document.getElementById('option2');
+let optionThree = document.getElementById('option3');
+let audio = document.getElementById('beep');
+let audio1 = document.getElementById('clap');
+optionOne.addEventListener('click', ()=>{
+    if(optionOne.innerHTML==answer){
+        audio1.play();
+    generateEquation();
+}else{
+  audio.play();  
+}
+});
+optionTwo.addEventListener('click', ()=>{
+    if(optionTwo.innerHTML==answer){
+        audio1.play();
+    generateEquation();
+}else{
+    audio.play();  
+}
+});
+optionThree.addEventListener('click', ()=>{
+    if(optionThree.innerHTML==answer){
+        audio1.play();
+    generateEquation();
+}else{
+    audio.play();   
+}
+});
+ function generateEquation(){
+var num1=Math.floor(Math.random() *(10 -1)+1);
+var num2=Math.floor(Math.random() *(10 -1)+1);
+if(num2 > num1){
+    num1 = num2;
+}
+var dummyAnswer=Math.floor(Math.random() *(20 -1)+0)
+var dummyAnswerOne=Math.floor(Math.random() *(20 -1)+0)
+
+     /*dummyAnswerOne.toString().split('').slice(0,2);
+     answer.toString().split('').slice(0,2);*/
+     var allAnswers=[];
+ answer=num1 / num2;
+document.getElementById('num1').innerHTML=num1;
+document.getElementById('num2').innerHTML=num2;
+allAnswers=[answer, dummyAnswer, dummyAnswerOne];
+ const shuffleAnswer=(arr)=>arr.sort(() =>Math.random() -0.5);
+ const list=shuffleAnswer(allAnswers); //shuffle the answers
+ optionOne.innerHTML=list[0];
+ optionTwo.innerHTML=list[1];
+ optionThree.innerHTML=list[2];
+ }
+ window.addEventListener('load', ()=>{
+    generateEquation();
+ })
